841f3c8467fa29829a7634a04684956b52f633a3
teddyhong <rusdlsla@gmail.com>
2018-09-04 19:17:09 +0900

    spring boot reference - Getting Started

--- src/main/java/io/celestialbox/Application.java
+++ src/main/java/io/celestialbox/Application.java
@@ -1,5 +1,20 @@
 package io.celestialbox;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
