c0c4f35a399b55418107a92e78aed2b819457bde
teddyhong <rusdlsla@gmail.com>
2018-09-04 20:03:02 +0900

    spring boot reference - Getting Started complete.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # springboot-sample
-springboot sample
+
+springboot 기본 셋팅
+
+### jar file 생성
+
+```
+$ gradle clean build -x test
+``` 
+
+### springboot 실행
+
+```
+$ java -jar build/libs/springboot-sample-0.1.0.jar
+```
+
+
--- src/main/java/io/celestialbox/myapplication/Application.java
+++ src/main/java/io/celestialbox/myapplication/Application.java
@@ -1,12 +1,12 @@
-package io.celestialbox;
+package io.celestialbox.myapplication;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     @RequestMapping("/")
@@ -14,7 +14,7 @@ public class Application {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
